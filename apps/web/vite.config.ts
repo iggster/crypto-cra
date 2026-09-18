cad09616d50074c3a069eefa177184a828d203c0
iggster <igna.murillo@gmail.com>
2026-09-18 19:53:08 -0400

    Configure TanStack Router

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,11 +1,16 @@
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    tanstackRouter({
+      routesDirectory: "./src/routes",
+      generatedRouteTree: "./src/routeTree.gen.ts",
+    }),
+    babel({ presets: [reactCompilerPreset()] }),
   ],
-})
+});
